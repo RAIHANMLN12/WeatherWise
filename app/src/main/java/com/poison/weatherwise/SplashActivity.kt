b1c43928899a41d2d28522f41493eabf53737756
Raihan Maulana <73742620+RAIHANMLN12@users.noreply.github.com>
2024-01-17 10:51:00 +0700

    add splash screen

---
+++ app/src/main/java/com/poison/weatherwise/SplashActivity.kt
@@ -0,0 +1,70 @@
+package com.poison.weatherwise
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen()
+        }
+    }
+
+    @Composable
+    private fun SplashScreen(){
+        LaunchedEffect(
+            key1 = true,
+            block = {
+                delay(3000)
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_app),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "WeatherWise",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black
+            )
+        }
+    }
+}
